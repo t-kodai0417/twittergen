32d5393a639dbc249bcc11c23379f16f5b1194e9
t-Kodai <75510055+t-kodai0417@users.noreply.github.com>
2022-01-18 13:47:15 +0900

    Update testing.py

--- testing.py
+++ testing.py
@@ -8,6 +8,23 @@ import random
 unicode1=(r'C:\Users\user\OneDrive\デスクトップ\gyazoapi\chromedriver.exe')
 browser = webdriver.Chrome(executable_path = unicode1)
 browser.implicitly_wait(3)
+#vpn
+print('15秒以内にvpnに接続しなさい。')
+time.sleep(15)
+#mail connect
+kukuru="https://m.kuku.lu"
+browser.get(kukuru)
+time.sleep(3)
+#期限付きのメルアドを取得
+kigen = browser.find_element(by=By.XPATH, value="/html/body/div[1]/div[2]/div/div/div[6]/div[1]/div[5]/a")
+kigen.click()
+time.sleep(1)
+#メルアドをコピー
+copymail=browser.find_element(by=By.XPATH, value="/html/body/div[1]/div[4]/div/div/div/div/div[4]/a")
+copymail.click()
+#タブを追加してそっちにいく
+browser.execute_script("window.open()")
+browser.switch_to.window(browser.window_handles[1])
 #twitterに接続
 url_login = 'https://twitter.com/i/flow/signup'
 print(f"Access:{url_login}")
@@ -26,10 +43,12 @@ twuser.send_keys(USER)
 changemail = browser.find_element(by=By.XPATH, value="/html/body/div/div/div/div[1]/div[2]/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div[1]/div/div[4]/span")
 changemail.click()
 #メルアド入力
-mail="aaa@aaa.aaa"
 mail_input="/html/body/div/div/div/div[1]/div[2]/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div[1]/div/div[3]/label/div/div[2]/div/input"
 tw_mail = browser.find_element(by=By.XPATH, value=mail_input)
-tw_mail.send_keys(mail)
+tw_mail.click()
+pyautogui.keyDown('ctrl')
+pyautogui.press(['v'])
+pyautogui.keyUp('ctrl')
 #誕生日等を入力
 #月
 select_element = browser.find_element(by=By.XPATH, value="/html/body/div/div/div/div[1]/div[2]/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div[1]/div/div[5]/div[3]/div/div[1]/select")
@@ -44,4 +63,46 @@ select_element = browser.find_element(by=By.XPATH, value="/html/body/div/div/div
 select_object = Select(select_element)
 select_object.select_by_index(25)
 #--------------------------------
+print('15秒以内にrecaptcha認証を完了させなさい。')
+time.sleep(15)
+browser_from = browser.find_element(by=By.XPATH, value="/html/body/div[1]/div[2]/form/div[2]/div[2]/button")
+browser_from.click()
+print("The button is pushed!")
+links = browser.find_element(by=By.XPATH, value="/html/body/div/div[3]/div[2]/div[2]/input")
+links.click()
+pyautogui.keyDown('ctrl')
+pyautogui.press(['c'])
+pyautogui.keyUp('ctrl')
+time.sleep(1)
+browser.get("https://discord.com/register")
+links = browser.find_element(by=By.XPATH, value="/html/body/div[1]/div[2]/div/div/div/form/div/div/div[1]/div/input")
+links.click()
+pyautogui.keyDown('ctrl')
+pyautogui.press(['v'])
+pyautogui.keyUp('ctrl')
+#print(random.randint(1000, 9999))
+random1=(random.randint(1000, 9999))
+USER=(f"Tinker{random1}")
+element=browser.find_element(by=By.XPATH, value="/html/body/div[1]/div[2]/div/div/div/form/div/div/div[2]/div/input")
+element.clear()
+element.send_keys(USER)
+password="kodai0417"
+element=browser.find_element(by=By.XPATH, value="/html/body/div[1]/div[2]/div/div/div/form/div/div/div[3]/div/input")
+element.clear()
+element.send_keys(password)
+year=("2000")
+element=browser.find_element(by=By.XPATH, value="/html/body/div[1]/div[2]/div/div/div/form/div/div/div[4]/div[1]/div[1]/div/div/div/div/div[1]/div[2]/div/input")
+element.clear()
+element.send_keys(year)
+month=("1")
+element=browser.find_element(by=By.XPATH, value="/html/body/div[1]/div[2]/div/div/div/form/div/div/div[4]/div[1]/div[2]/div/div/div/div/div[1]/div[2]/div/input")
+element.clear()
+element.send_keys(month)
+days=("11")
+element=browser.find_element(by=By.XPATH, value="/html/body/div[1]/div[2]/div/div/div/form/div/div/div[4]/div[1]/div[3]/div/div/div/div/div[1]/div[2]/div/input")
+element.clear()
+element.send_keys(days)
+links = browser.find_element(by=By.XPATH, value="/html/body/div[1]/div[2]/div/div/div/form/div/div/div[5]/button")
+links.click()
+print('認証しなさい。')
 time.sleep(30)
